c783b54a20b1a82333bac446e53ace2583b10a98
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-11-12 19:21:07 -0300

    começando OO

--- IFNetwork/model/post.py
+++ IFNetwork/model/post.py
@@ -1,20 +1,28 @@
 import sqlite3
-conn = sqlite3.connect("IFNetwork.db")
-cursor = conn.cursor()
 
-texto_post = input("Qual a mensagem? ")
-id_usuario = int(input("Quem envia? "))
-visibilidade = input("Pra quem envia? ")
+class Post():
 
-cursor.execute("""
-INSERT INTO post(texto_post, id_usuario, visibilidade) VALUES (?, ?, ?);
-""", (texto_post, id_usuario, visibilidade))
+    def __init__ (texto_post, id_usuario, visibilidade):
+        self.texto_post = texto_post
+        self.id_usuario = id_usuario
+        self.visibilidade = visibilidade
 
-cursor.execute("""
-SELECT * FROM post """)
+    def postar():
+        conn = sqlite3.connect("IFNetwork.db")
+        cursor = conn.cursor()
 
-for linha in cursor.fetchall():
-    print(linha)
+        texto_post = input("Qual a mensagem? ")
+        id_usuario = int(input("Quem envia? "))
+        visibilidade = input("Pra quem envia? ")
+
+        cursor.execute("""
+        INSERT INTO tb_post(texto_post, id_usuario, visibilidade) VALUES (?, ?, ?);
+        """, (texto_post, id_usuario, visibilidade))
+
+        cursor.execute(""" SELECT * FROM Post """)
+
+        for linha in cursor.fetchall():
+            print(linha)
 
 conn.commit()
 
