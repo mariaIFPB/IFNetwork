370438876892990cc313ffdf45c9189fef7bd4ee
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-11-12 19:07:09 -0300

    começando OO

--- IFNetwork/model/usuario.py
+++ IFNetwork/model/usuario.py
@@ -1,20 +1,46 @@
 import sqlite3
 
-conn = sqlite3.connect('IFNetwork.db')
-cursor = conn.cursor()
+class Usuario():
+    def __init__(self, nome, email, senha, genero, idade, profissao, cidade, usuarios):
+        usuarios = []
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.genero = genero
+        self.idade = idade
+        self.profissao = profissao
+        self.cidade = cidade
 
-nome = input("informe seu nome: ")
-email = input("informe seu email: ")
-senha = input("informe a senha: ")
-genero = input("informe o seu gênero: ")
-idade = int(input("informe sua idade: "))
-profissao = input("informe sua profissão: ")
-cidade = input("informe sua cidade: ")
+    def inserir(self):
+        conn = sqlite3.connect('IFNetwork.db')
+        cursor = conn.cursor()
+        nome = input("informe seu nome: ")
+        email = input("informe seu email: ")
+        senha = input("informe a senha: ")
+        genero = input("informe o seu gênero: ")
+        idade = int(input("informe sua idade: "))
+        profissao = input("informe sua profissão: ")
+        cidade = input("informe sua cidade: ")
 
-cursor.execute("""
-INSERT INTO usuario(nome, email, senha, genero, idade, profissao, cidade)
-VALUES (?, ? ,? ,? ,? ,? ,?);
-""", (nome, email, senha, genero, idade, profissao, cidade))
+        cursor.execute("""
+            INSERT INTO tb_usuario(nome, email, senha, genero, idade, profissao, cidade)
+            VALUES (self.nome, self.email, self.senha, self.genero, self.idade, self.profissao, self.cidade);
+            """)
+
+    def listar(usuario):
+        conn = sqlite3.connect('IFNetwork.db')
+        cursor = conn.cursor()
+        cursor.execute(""" SELECT * FROM usuario """)
+
+        for linha in cursor.fetchall():
+            self.nome = nome
+            print(linha)
+            usuario = Usuario(nome, email, senha, genero, idade, profissao, cidade, usuarios)
+            usuarios.append(usuario)
+
+        return usuarios
+
+
 
 conn.commit()
 
