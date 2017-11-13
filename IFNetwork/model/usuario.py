57d50975e0047cc04bf8a115c4d51d854ade1420
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-11-12 21:05:38 -0300

    começando oo

--- IFNetwork/model/usuario.py
+++ IFNetwork/model/usuario.py
@@ -26,24 +26,51 @@ class Usuario():
             INSERT INTO tb_usuario(nome, email, senha, genero, idade, profissao, cidade)
             VALUES (self.nome, self.email, self.senha, self.genero, self.idade, self.profissao, self.cidade);
             """)
-
+        conn.commit()
+        conn.close()
+        
     def listar(usuario):
         conn = sqlite3.connect('IFNetwork.db')
         cursor = conn.cursor()
         cursor.execute(""" SELECT * FROM usuario """)
 
         for linha in cursor.fetchall():
-            self.nome = nome
-            print(linha)
+            nome = linha[1]
+            email = linha[2]
+            senha = linha[3]
+            profissao = linha[4]
+            sexo = linha[5]
             usuario = Usuario(nome, email, senha, genero, idade, profissao, cidade, usuarios)
             usuarios.append(usuario)
-
+            conn.commit()
+            conn.close()
+            
         return usuarios
 
-
-
-conn.commit()
-
-print("Usuário cadastrado com sucesso")
-
-conn.close()
+    def atualizar_fone_email(self, id):
+        conn = sqlite3.connect('IFNetwork.db')
+        cursor = conn.cursor()
+        self.novo_fone = input('Fone: ')
+        self.novo_email = input('Email: ')
+        cursor.execute("""
+        UPDATE tb_usuario
+        SET fone = ?
+        WHERE id = ?
+        """, (self.novo_fone, id,))
+        cursor.execute("""
+        UPDATE tb_usuario
+        SET email = ?
+        WHERE id = ?
+        """, (self.novo_email, id,))
+        print("Dados atualizados com sucesso.")
+        conn.commit()
+        conn.close()
+        
+    def deletar(self, id):
+        conn = sqlite3.connect('IFNetwork.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM tb_usuario WHERE id = ?
+            """, (id))
+        conn.commit()
+        conn.close()
