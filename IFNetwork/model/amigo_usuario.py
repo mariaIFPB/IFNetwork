7d980eef3b5529e60b0e481139283d1117a10b92
yohannatethysy <thetysy@gmail.com>
2017-11-19 11:47:46 -0300

    Update amigo_usuario.py

--- IFNetwork/model/amigo_usuario.py
+++ IFNetwork/model/amigo_usuario.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 class Amigo_Usuario():
-    def __init__ (email1, email2):
+    def __init__(self, email1, email2):
         self.email1 = email1
         self.email2 = email2
 
@@ -12,7 +12,7 @@ class Amigo_Usuario():
         email2 = int(input("informe o email do seu amigo: "))
 
         cursor.execute("""
-        INSERT INTO amigo_usuario(email1, email2)
+        INSERT INTO tb_amigo_usuario(email1, email2)
         VALUES (?,?); """, (email1, email2))
 
         conn.commit()
