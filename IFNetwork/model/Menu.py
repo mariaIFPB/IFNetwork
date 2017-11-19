97174c326f366cffa1f0746d92ed00c622dfc756
yohannatethysy <thetysy@gmail.com>
2017-11-19 11:45:48 -0300

    Update Menu.py

--- IFNetwork/model/Menu.py
+++ IFNetwork/model/Menu.py
@@ -7,18 +7,21 @@ opcao = int(input("seja bem vindo!\n"
       "1 - criar novo usuário\n"
       "2 - deletar usuário\n")
 
-if (opcao == 1):
-    nome = input("informe seu nome: ")
-    email = input("informe seu email: ")
-    senha = input("informe a senha: ")
-    genero = input("informe o seu gênero: ")
-    idade = int(input("informe sua idade: "))
-    profissao = input("informe sua profissão: ")
-    cidade = input("informe sua cidade: ")
-    u = Usuario(nome, email, senha, genero, idade, profissao, cidade, usuarios)
-    u.inserirUser()
+try:
+    if (opcao == 1):
+        nome = input("informe seu nome: ")
+        email = input("informe seu email: ")
+        senha = input("informe a senha: ")
+        genero = input("informe o seu gênero: ")
+        idade = int(input("informe sua idade: "))
+        profissao = input("informe sua profissão: ")
+        cidade = input("informe sua cidade: ")
+        u = Usuario(nome, email, senha, genero, idade, profissao, cidade)
+        u.inserirUser()
 
-elif (opcao == 2)
-    email = input("informe o email da conta a ser deletada: ")
-    u.
+    elif(opcao == 2):
+        email = input("informe o email da conta a ser deletada: ")
+        u.deletar()
+except:
+    print("opção inválida.")
 
