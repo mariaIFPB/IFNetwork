dcd3909e665bf5e914dd049318968794491e2a43
yohannatethysy <thetysy@gmail.com>
2017-11-19 17:20:22 -0200

    Update amizade.py

--- IFNetwork/model/amizade.py
+++ IFNetwork/model/amizade.py
@@ -1,17 +1,21 @@
 import sqlite3
-import datetime
+from datetime import *
 
 class Amizade():
+    def __init__(self, dataAmizde):
+        self.dataAmizade = dataAmizde
+
+    dataAmizade = datetime('now')
 
     conn = sqlite3.connect("IFNetwork.db")
     cursor = conn.cursor()
 
     cursor.execute("""
-    INSERT INTO amizade(data_amizade) VALUES(datetime('now'));
+    INSERT INTO tb_amizade(data_amizade) VALUES(dataAmizade);
     """)
 
     conn.commit()
-
+    
     print("amizade funciona.")
 
     conn.close()
