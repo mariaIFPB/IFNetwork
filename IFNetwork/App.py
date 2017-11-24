0d777726b1942e3e4b4dad8f9eb30c70ec2cf54f
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-11-24 12:28:31 -0200

    atualização

--- IFNetwork/App.py
+++ IFNetwork/App.py
@@ -1,55 +1,63 @@
 import sqlite3
 
-conn = sqlite3.connect('IFNetwork.db')
-cursor = conn.cursor()
-
 # Criando as tabelas: usuario, mensagem, post, amizade, amigo_usuario
 
-cursor.execute("""
-    CREATE TABLE tb_usuario(
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        senha VARCHAR(15) NOT NULL,
-        genero VARCHAR(15),
-        idade INTEGER,
-        profissao VARCHAR(50),
-        cidade VARCHAR(50),
-        );
-        """)
+try:
 
-cursor.execute("""
-    CREATE TABLE tb_amizade(
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        data_amizade TEXT
-        );
-        """)
+    conn = sqlite3.connect('IFNetwork.db')
+    cursor = conn.cursor()
 
-cursor.execute("""
-    CREATE TABLE tb_amigo_usuario(
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        id_usuario1 INTEGER,
-        id_usuario2 INTEGER,
-        );
-        """)
+    cursor.execute("""
+        CREATE TABLE tb_usuario(
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            senha VARCHAR(15) NOT NULL,
+            genero VARCHAR(15),
+            idade INTEGER,
+            profissao VARCHAR(50),
+            cidade VARCHAR(50)
+            );
+            """)
 
-cursor.execute("""
-    CREATE TABLE tb_mensagem(
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        id_remetente INTEGER FOREIGN KEY,
-        id_destinatario INTEGER FOREIGN KEY,
-        texto_msg(255),
-        horario_MSG DATE)
-        );
-        """)
+    cursor.execute("""
+        CREATE TABLE tb_amizade(
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            data_amizade TEXT
+            );
+            """)
 
-cursor.execute("""
-    CREATE TABLE tb_post(
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        id_usuario INTEGER NOT NULL,
-        texto_post VARCHAR(150) NOT NULL,
-        visibilidade VARCHAR(250)
-        );
-        """)
+    cursor.execute("""
+        CREATE TABLE tb_amigo_usuario(
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            id_usuario1 INTEGER,
+            id_usuario2 INTEGER
+            );
+            """)
 
-conn.close()
+    cursor.execute("""
+        CREATE TABLE tb_mensagem(
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            id_remetente INTEGER ,
+            id_destinatario INTEGER FOREIGN KEY,
+            texto_msg(255),
+            horario_MSG DATE
+            id_remetente FOREIGN KEY,
+            REFERENCES usuario(id),
+            id_destinatario FOREIGN KEY,
+            REFERENCES usuario(id)
+            );
+            """)
+
+    cursor.execute("""
+        CREATE TABLE tb_post(
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            id_usuario INTEGER NOT NULL,
+            texto_post VARCHAR(150) NOT NULL,
+            visibilidade VARCHAR(250)
+            );
+            """)
+    conn.close()
+
+except sqlite3.OperationalError:
+    print("o banco de dados ja existe.")
