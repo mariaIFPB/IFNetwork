dedabf480c8983de326622c65b484e4ed3e02182
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-11-30 11:34:35 -0300

    Update post.py

--- IFNetwork/model/post.py
+++ IFNetwork/model/post.py
@@ -2,25 +2,29 @@ import sqlite3
 
 class Post():
 
-    def __init__(self, texto_post, id_usuario, visibilidade):
+    def __init__(self, texto_post, email_usuario, visibilidade):
         self.texto_post = texto_post
-        self.id_usuario = id_usuario
+        self.email_usuario = email_usuario
         self.visibilidade = visibilidade
 
+    def visibilidadePost(self):
+        #resolver se será público, só para amigos, só para um amigo.
+        pass
+    
     def postar(self):
         feed = []
         conn = sqlite3.connect("IFNetwork.db")
         cursor = conn.cursor()
 
         texto_post = input("Qual a mensagem? ")
-        id_usuario = int(input("Quem envia? "))
-        visibilidade = input("Pra quem envia? ")
-        post = Post(texto_post, id_usuario, visibilidade)
+        email_usuario = input("Quem envia? ")
+        visibilidade = visibilidadePost(self)
+        post = Post(texto_post, email_usuario, visibilidade)
         feed.append(post)
         
         cursor.execute("""
-        INSERT INTO tb_post(texto_post, id_usuario, visibilidade) VALUES (?, ?, ?);
-        """, (texto_post, id_usuario, visibilidade))
+        INSERT INTO tb_post(texto_post, email_usuario, visibilidade) VALUES (?, ?, ?);
+        """, (texto_post, email_usuario, visibilidade))
 
         cursor.execute(""" SELECT * FROM Post """)
 
